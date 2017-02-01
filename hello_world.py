fbc6184518383087b065d0ad79d74f39872bf397
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-01-31 23:50:20 -0600

    print y tipos de datos

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+print("Hola, me llamo Héctor")
+print("Soy sensei de cinta roja")
+print("Tengo 27")
+print("Hello world!")
---
+++ tipos_de_datos.py
@@ -0,0 +1,15 @@
+# entero
+6
+
+# esto es una floatante
+
+4.0
+5.0
+90e10
+
+# esto es un string
+
+"hola"
+
+# esto es una variable
+nombre = "Héctor"
