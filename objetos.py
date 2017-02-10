ef8eea3927fe732c4dee00fb76aeb3ae284e0d07
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-02-09 23:55:43 -0600

    Ejercicio de objetos

--- objetos.py
+++ objetos.py
@@ -4,14 +4,19 @@ class Animal:
     n_patas = 4
     color = "negro"
     sonido = ""
+
     def hacer_sonido(self):
         print(self.sonido)
 
+    def correr(self):
+        print("run run run")
 
 
 perro = Animal()
 gato = Animal()
 print(perro.sonido)
 perro.sonido = "Gua gua!!!!"
+perro.color = "amarillo"
+print(perro.sonido)
 perro.hacer_sonido()
 # mi_diccionario.iteritems()
